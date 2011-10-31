026a37d4f09ee86e24f9444a454da8d37fd1f284
David S <soulnafein@gmail.com>
2011-10-31 20:35:16 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roleplayingtable::Application.config.secret_token = 'b92052e4ba42dd715caf763bd4cd14d6ae0cf859d3141801f1f4a0f0cb3fea6b07127dd70a67b66514d0cf871d9e9c8ef37a09ad5f22a8215db1f6a8b3e75352'
